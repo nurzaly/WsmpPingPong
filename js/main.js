6aad03dfacfa7aa25676239097927180c7bdae64
Nurzaly <nrzaly@gmail.com>
2022-05-17 12:56:30 +0800

    update paddle

--- js/main.js
+++ js/main.js
@@ -1,44 +1,74 @@
 const $board = $('#board');
 const $ball = $('#ball');
 const $computerPaddle = $('#computer-paddle');
+const $playerPaddle = $('#player-paddle');
 
 const UP_LEFT = -3 * Math.PI / 4;
 const UP_RIGHT = - Math.PI / 4;
 const DONW_LEFT = 3 * Math.PI / 4;
 const DOWN_RIGHT = Math.PI / 4;
 
+const PADDLE_UP = -1;
+const PADDLE_DOWN = 1;
+
 let ball = null;
 let interval = null;
 let computerPaddle = null;
+let playerPaddle = null;
 
-function setup(){
-    computerPaddle = {
-        direction: 1,
-        SPEED: 5,
-        top: $board.height()/2 - $computerPaddle.height()/2
-    };
+computerPaddle = {
+    direction: PADDLE_UP,
+    SPEED: 5,
+    top: $board.height()/2 - $computerPaddle.height()/2
+};
+
+playerPaddle = {
+    direction: 1,
+    SPEED: 50,
+    top: $board.height()/2 - $playerPaddle.height()/2
+};
+
+
+document.onkeydown = function(e){
+    switch (e.which){
+        case 38: //key up
+        playerPaddle.direction = PADDLE_UP;
+        break;
+
+        case 40: // key down
+        playerPaddle.direction = PADDLE_DOWN;
+        break;
+    }
+
+    updatePlayerPaddle();
+}
+
+function init(){
 
     ball = {
         top: 200,
         left: 200,
-        speed: 5,
+        speed: 10,
         angle: UP_LEFT
     };
 
     $computerPaddle.css({
         top:`${computerPaddle.top}px`
-    })
+    });
+
+    $playerPaddle.css({
+        top:`${playerPaddle.top}px`
+    });
+
+    startGame();
 }
 
-function init(){
-
-    
-
-    interval = setInterval(update, 30);
+function startGame(){
+    interval = setInterval(update, 20);
 }
 
 function update(){
-    //updateComputerPaddle();
+    updateComputerPaddle();
     updateBall();
 }
 
@@ -52,7 +82,7 @@ function updateBall(){
         left: `${ball.left}px`
     });
 
-    if(isBallOverlapTop()){
+    if(isBallHitTop()){
         if(ball.angle === UP_RIGHT){
             ball.angle = DOWN_RIGHT
         }
@@ -60,6 +90,51 @@ function updateBall(){
             ball.angle = DONW_LEFT
         }
     }
+
+    if(isBallHitLeft()){
+        if(ball.angle === DONW_LEFT){
+            ball.angle = DOWN_RIGHT;
+        }
+        else{
+            ball.angle = UP_RIGHT
+        }
+    }
+
+    if(isBallHitBottom()){
+        if(ball.angle === DOWN_RIGHT){
+            ball.angle = UP_RIGHT;
+        }
+        else{
+            ball.angle = UP_LEFT;
+        }
+    }
+
+    if(isBallHitRight()){
+        if(ball.angle === UP_RIGHT){
+            ball.angle = UP_LEFT;
+        }
+        else{
+            ball.angle = DONW_LEFT;
+        }
+    }
+
+}
+
+function updatePlayerPaddle(){
+
+    playerPaddle.top += playerPaddle.direction * playerPaddle.SPEED;
+
+    if(playerPaddle.top <= 0){
+        playerPaddle.top = 0;
+    }
+
+    if(playerPaddle.top >= $board.height() - $playerPaddle.height()){
+        playerPaddle.top = $board.height() - $playerPaddle.height();
+    }
+
+    $playerPaddle.css({
+        top:`${playerPaddle.top}px`
+    });
 
 }
 
@@ -84,15 +159,23 @@ function updateComputerPaddle(){
 
 
 
-function isBallOverlapTop(){
+function isBallHitTop(){
     return ball.top <= 0;
 }
 
-function ifBallOverLapLeft(){
+function isBallHitLeft(){
     return ball.left <= 0;
 }
 
-setup();
+function isBallHitBottom(){
+    return ball.top >= $board.height() - $ball.height();
+}
+
+function isBallHitRight(){
+    return ball.left >= $board.width() - $ball.width();
+}
+
+init();
 
 
 
